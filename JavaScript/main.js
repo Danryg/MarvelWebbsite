3d28090cf5d02ec43711fe59f541b717278d1811
DanielRygaard <daniel.rygaard@live.se>
2020-11-29 13:12:07 +0100

    Cleaning upp spagetti

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -5,140 +5,9 @@
 var $Content;
 var AllMovies;
 var $moviesFromStart;
-$('#search-input').on('keyup', function(){
-    var value = $(this).val();
-    console.log(value);
-});
-
-function getAllMovies(){
-
-    $.ajax({
-        type:'GET',
-        url: 'http://192.168.1.150:3000/api/movies',
-        success: (movies)=>{
-            placeAllMovies(movies, "");
-            $moviesFromStart = movies;
-        }
-    });
-}
-
-
-
-
-function placeAllMovies(movies, value){
-
-    $Content.find('ul').remove();
-    removeButtons();
-
-    $Content.append('<ul  class="ContentList" id="panel1"></ul>');
-    var $movielist = $('#panel1');
-    $movielist.append("<li><div class='bg'></div><p id='title'>Test</p></li>");
-
-
-
-    var $LiElement = $movielist.find('li');
-    var $LiWidth = $LiElement.outerWidth(true);
-    var $LiHeight = $LiElement.outerHeight(true);
-
-    $movielist.find('li').remove();
-
-
-    var $PanelWidth = $movielist.width();
-    var $PanelHeight = $movielist.height();
-
-    var $maxElementsPerRow = Math.floor( $PanelWidth/$LiWidth);
-    var $maxRows = Math.floor($PanelHeight/$LiHeight);
-
-
-    var $maxElementsPerPage = $maxRows * $maxElementsPerRow;
-
-
-
-    console.log("Max Rows = " + $maxRows + "   max elements per row = " + $maxElementsPerRow + " Max elements: " + $maxElementsPerPage);
-
-
-    var o = 0;
-    var panelNumber = 1;
-    var $currentList = $movielist;
-    addButton(1);
-    $.each(movies, (i, movie)=>{
-
-        console.log(movie.title.toLowerCase().indexOf(value));
-        if(!(movie.title.toLowerCase().indexOf(value) > -1)){
-            return;
-        }
-
-        if(o < $maxElementsPerPage){
-        $currentList.append("<li><div class='bg'><p id='title'>"+movie.title+"</p><p id='desc'>"+movie.desc+"</p><div class='container'><a href='' class='btn btn-2'>more</a></div></li>");
-
-
-        }else{
-            console.log("i: " +i);
-            if(i == movies.length){
-                return false;
-            }
-            panelNumber++;
-            o = 0;
-
-            $Content.append("<ul class'ContentList' id='panel"+panelNumber+"'></ul>");
-            var id = "#panel" +panelNumber;
-
-            $currentList = $Content.find(id);
-
-            console.log($currentList +"    i: " + o);
-            addButton(panelNumber);
-
-            $currentList.append("<li><div class='bg'><p id='title'>"+movie.title+"</p><p id='desc'>"+movie.desc+"</p><div class='container'><a href='' class='btn btn-2'>more</a></div></li>");
-
-        }
-
-       o++;
-
-    });
-}
-
-function addButton(number){
-    var $buttonContainer = $Content.find('#pageButtons');
-
-    $buttonContainer.append("<div class='pButton' data-panelid='panel"+number+"'>"+number+"</div>");
-    addButtonFucntionality($buttonContainer);
-
-}
-
-function addButtonFucntionality(buttonContainer){
-    $buttons = buttonContainer.find('.pButton');
-
-    $buttons.on("click", function(){
-        hideAllPanels();
-        var panelid = $(this).attr('data-panelid');
-        panelid = '#' +panelid;
-        console.log(panelid);
-        $Content.find(panelid).show();
-
-    });
-}
-
-function hideAllPanels(){
-    $panels = $Content.find('ul');
-    $panels.hide();
-}
-
-function addMovie(title, desc){
-
-
-}
-
-function removeButtons(){
-    var $buttonContainer = $Content.find('#pageButtons');
-    $buttonContainer.find("div").remove();
-}
-function onResize(){
-    getAllMovies();
-}
 
 
 $(function (){
-
 
 
     $Content = $('#Content');
@@ -169,36 +38,178 @@ $(function (){
 
 });
 
-/*
-    Side note. preferably if we could calculate how many elemnts there can be in one "page"
-*   Checks if there is overlflow in the list, if there is put items on another page untill it isn't
-*   Add buttons based on how many pages there are
-*
-*    ADD A PAGE
-*   Add a ul with class panel to #content to to make a page.
-*   ul
-*
-*    ADD AN ITEM
-*   Add a li with tag movieItem with a div inside with class bg and a
-*   tag with class title
-*   li>div.bg+p.title
-*/
-function thing(){
-    $listcontainer = $(".ContentList");
-    $item = $(".item");
-    width = $listcontainer.width();
-    height = $listcontainer.height();
+function getAllMovies(){
 
-    ItemMargin = 20;
-    ItemWidth = $item.width();
-    ItemHeight = $item.height();
+    $.ajax({
+        type:'GET',
+        url: 'http://192.168.1.150:3000/api/movies',
+        success: (movies)=>{
+            placeAllMovies(movies, "");
+            $moviesFromStart = movies;
+        }
+    });
+}
 
 
 
 
+function placeAllMovies(movies, value){
 
+    $Content.find('ul').remove();
+    removeButtons();
+
+    $Content.append('<ul  class="ContentList" id="panel1"></ul>');
+    var $movielist = $('#panel1');
+    addMovie($movielist, "Test");
+    var $LiElement = $movielist.find('li');
+
+
+    var calc = new PanelAndItemCalculator($movielist, $LiElement);
+
+
+    $movielist.find('li').remove();
+
+
+
+    var $maxElementsPerRow = calc.getMaxElementsPerRow();
+    var $maxRows = calc.getMaxRows();
+
+
+    var $maxElementsPerPage = $maxRows * $maxElementsPerRow;
+
+
+
+
+
+
+    var o = 0;
+    var panelNumber = 1;
+    var $currentList = $movielist;
+    addButton(1);
+    $.each(movies, (i, movie)=>{
+
+
+        if(ifMovieHasLetters(movie,value)){
+            return;
+        }
+
+        if(o < $maxElementsPerPage){
+        addMovie($currentList, movie);
+
+
+        }else{
+            console.log("i: " +i);
+            if(i == movies.length){
+                return false;
+            }
+            panelNumber++;
+            o = 0;
+
+            addPanel(panelNumber);
+            var id = "#panel" +panelNumber;
+
+            $currentList = $Content.find(id);
+
+
+            addButton(panelNumber);
+            addMovie($currentList, movie);
+
+        }
+
+       o++;
+
+    });
+}
+
+class PanelAndItemCalculator{
+
+
+    constructor(panel, item){
+        this.$LiWidth = item.outerWidth(true);
+        this.$LiHeight = item.outerHeight(true);
+        this.$PanelWidth = panel.width();
+        this.$PanelHeight = panel.height();
+
+    }
+
+    getMaxElementsPerRow(){
+        var $maxElementsPerRow = Math.floor( this.$PanelWidth/this.$LiWidth);
+        return $maxElementsPerRow;
+
+    }
+
+    getMaxRows(){
+        var $maxRows = Math.floor(this.$PanelHeight/this.$LiHeight);
+        return $maxRows;
+    }
+
+    getMaxElementsPerPage(){
+        var $maxElementsPerPage = this.getMaxRows() * this.getMaxElementsPerRow();
+        return $maxElementsPerPage;
+    }
 
 }
 
+
+function ifMovieHasLetters(movie, value){
+    if(!(movie.title.toLowerCase().indexOf(value) > -1)){
+            return true;
+        }
+
+    return false;
+
+}
+
+function addPanel(panelNumber){
+    $Content.append("<ul class'ContentList' id='panel"+panelNumber+"'></ul>");
+}
+
+function addMovie(list, movie){
+    list.append("<li><div class='bg'><p id='title'>"+movie.title+"</p><p id='desc'>"+movie.desc+"</p><div class='container'><a href='' class='btn btn-2'>more</a></div></li>");
+}
+
+function addButton(number){
+    var $buttonContainer = $Content.find('#pageButtons');
+
+    $buttonContainer.append("<div class='pButton' data-panelid='panel"+number+"'>"+number+"</div>");
+    addButtonFucntionality($buttonContainer);
+
+}
+function addButtonFucntionality(buttonContainer){
+    $buttons = buttonContainer.find('.pButton');
+
+    $buttons.on("click", function(){
+        hideAllPanels();
+        var panelid = $(this).attr('data-panelid');
+        panelid = '#' +panelid;
+        console.log(panelid);
+        $Content.find(panelid).show();
+
+    });
+}
+function removeButtons(){
+    var $buttonContainer = $Content.find('#pageButtons');
+    $buttonContainer.find("div").remove();
+}
+
+function hideAllPanels(){
+    $panels = $Content.find('ul');
+    $panels.hide();
+}
+
+
+function onResize(){
+    getAllMovies();
+}
+
+
+
+
+/*
+TODOs
+Add Filters
+- Sort by Name
+- Sort by
+*/
 
 
